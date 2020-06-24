7640c73d17a69868ef7f666ac8a8bc7a47f9f71f
Jhonny Quispe <jhonny.quispejl@gmail.com>
2020-06-24 11:27:43 -0500

    Templates generados

---
+++ src/pages/Error404.js
@@ -0,0 +1,13 @@
+const Error404 = () => {
+	const view = `
+    <div class=Error404">
+      <h2>Error 404</h2>
+
+      <img src="./images/page_not_found.svg" />
+    </div>
+  `
+
+	return view
+}
+
+export default Error404
