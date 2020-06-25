80fcd75a5b4a92761176dcea4f20977f6120d4d1
Jhonny Quispe <jhonny.quispejl@gmail.com>
2020-06-25 17:01:50 -0500

    fix bugs

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -3,7 +3,7 @@ const Error404 = () => {
     <div class="Error404">
       <h2>Error</h2>
 
-      <img src="../src/images/page_not_found.svg" />
+      <img src="./images/page_not_found.svg" />
     </div>
   `
 
